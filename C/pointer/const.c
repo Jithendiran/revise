f56a39a2e1b0ce5e134cef15d0604bd6840bfa7e
jithendiran <jidesh0410@gmail.com>
2023-02-15 18:48:46 +0530

    constant pointers updated

---
+++ C/pointer/const.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void main()
+{
+    int a = 1, *ptr = &a;  // ptr is a pointer to integer
+    printf("a = %d\n", a); // a = 1
+    a = 2;
+    printf("a = %d\n", a); // a = 2
+    *ptr = 3;
+    printf("a = %d\n", a); // a = 3
+
+    const int b = 2;
+    int *ptrb = &b;        // ptrb is a pointer to integer
+    printf("b = %d\n", b); // b = 2
+    // b = 3; error
+    *ptrb = 3;
+    printf("b = %d\n", b); // b = 3
+
+    // ptrbc is a integer pointer whose value not change (const is applied to int which means value is not changeable)
+    const int *ptrbc = &b;      // or int const *ptrbc = &b;
+    printf("b = %d\n", *ptrbc); // b = 3
+    //*ptrbc = 4;                 // error
+    ptrbc = &a;
+    printf("ptrbc = %d \n", *ptrbc); // ptrbc = 3
+
+    // ptrd is an constant pointer whose address is not change (const is applied to ptrd which means address is not changeable )
+    int *const ptrd = &b;
+    // ptrd = &a; //error
+    printf("ptrd = %d \n", *ptrd); // ptrbc = 3
+    *ptrd = 10;
+    printf("B = %d\n", b); // B = 10
+
+    // ptre is an constant pointer to constant integer whose address and value is not changeable
+    const int *const ptre = &a;
+    // ptre = &b; // error
+    //*ptre = 20; // error
+}
