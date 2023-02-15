8c8015e7dd7b602d6289d6bf571976aeae769f54
jithendiran <jidesh0410@gmail.com>
2023-02-15 18:21:51 +0530

    until union

---
+++ C/enum.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+// enumeration in c is also an user defined data type
+enum Bool
+{
+    False,
+    True
+}; // if value not specified it start with 0
+enum Test
+{
+    a = 1,
+    b
+};
+
+enum Test1 // two or mor enum can share same value
+{
+    a1 = 3, // if a and b name is used  (redeclaration of enumerator ‘a’ error will be shown)
+    b1 = 3,
+    c, // c will get value 4 values is calculted based up on previous + 1
+    d = 2,
+    // e = 3.5 only int values are allowed
+};
+void main()
+{
+    enum Bool var;
+    var = False;
+    printf("Var : %d\n", var); // Var : 0
+    var = True;
+    printf("Var : %d\n", var); // Var : 1
+    var = 0;
+    printf("Var : %d\n", var); // Var : 0
+    var = 1;
+    printf("Var : %d\n", var); // Var : 1
+
+    var = 3;
+    printf("Var : %d\n", var); // Var : 3
+
+    enum Test var1;
+    var1 = a;
+    printf("Var1 : %d\n", var1); // Var1 : 1
+    var1 = b;
+    printf("Var1 : %d\n", var1); // Var1 : 2
+
+    enum Test1 var2;
+    var2 = a1;
+    printf("Var2 : %d\n", var2); // Var2 : 3
+    var2 = b1;
+    printf("Var2 : %d\n", var2); // Var2 : 3
+    var2 = c;
+    printf("Var2 : %d\n", var2); // Var2 : 4
+
+    var1 = c;
+    printf("Var1 : %d\n", var1); // Var1: 4
+
+    int a = True;
+    printf("A = %d\n", a); // A = 1
+}
---
+++ C/union/intro.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * Union are sharing same memory location
+ * size of the union is taken according to the largest member of the union
+ */
+union a
+{
+    int a;
+    char b;
+    int c;
+};
+
+void main()
+{
+    union a var, *ptr;
+    var.a = 65;
+    printf("int : %d, char : %c\n", var.a, var.b);                        // int : 65, char : A
+    printf("Size of Union : %d\n", sizeof(var));                          // Size of Union : 4
+    printf("Address of var = %p, a = %p,b = %p\n", &var, &var.a, &var.b); // Address of var = 0x7fffffffdc9c, a = 0x7fffffffdc9c,b = 0x7fffffffdc9c
+
+    /**
+     * assume var start address is 1000
+     *
+     * int has larger size that char so union's int size (4 byte)
+     * a,b,c has start address of 1000 and b is end in 1001 but a,c address end in 1004
+     */
+    // pointer access are same as structure
+    ptr = &var;
+    printf("Pointer access of c = %d\n", ptr->c); // Pointer access of c = 65
+}
